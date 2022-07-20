0a992a9bfd0c082eb8ec45e2cc87bd57b4a7f3d4
samyu1204 <87253028+samyu1204@users.noreply.github.com>
2022-07-20 17:43:30 +1000

    XGBoost

--- data_cv/model.py
+++ data_cv/model.py
@@ -2,16 +2,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
 import cleaning_helper
 import df
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+import xgboost as xgb
 
-# ['D_58', 'B_37', 'B_9', 'D_74', 'B_3', 'D_55', 'B_22', 'B_33', 'R_1', 'B_38', 'D_75', 'D_44', 'D_48', 'B_2', 'B_7', 'B_18', 'B_30', 'B_1', 'D_61', 'B_4', 'P_2', 'R_10', 'B_17', 'B_23']
+# cov_list = ['customer_ID', 'D_58', 'B_37', 'B_9', 'D_74', 'B_3', 'D_55', 'B_22', 'B_33', 'R_1', 'B_38', 'D_75', 'D_44', 'D_48', 'B_2', 'B_7', 'B_18', 'B_30', 'B_1', 'D_61', 'B_4', 'P_2', 'R_10', 'B_17', 'B_23']
+cov_list = ['customer_ID', 'B_30', 'B_1', 'B_23', 'B_7', 'D_55', 'B_37', 'B_17', 'B_3', 'D_58', 'D_61', 'B_4', 'B_22', 'D_44', 'D_75', 'R_1', 'B_9', 'B_38', 'D_48', 'R_10', 'D_74']
+
 def data_preprocessing():
   data_frame = df.get_par_training_data()
   # Data analysis for column "s":
-  data = data_frame[['customer_ID', 'D_58', 'B_37', 'B_9', 'D_74', 'B_3', 'D_55', 'B_22', 'B_33', 'R_1', 'B_38', 'D_75', 'D_44', 'D_48', 'B_2', 'B_7', 'B_18', 'B_30', 'B_1', 'D_61', 'B_4', 'P_2', 'R_10', 'B_17', 'B_23']]
+  data = data_frame[cov_list]
 
   # Fill all NA in the dataframe with 0's
   data = data.fillna(0)
@@ -25,21 +29,47 @@ def data_preprocessing():
 
 def test_data_process():
   data_frame = df.get_test_data()
-  data = data_frame[['customer_ID', 'D_58', 'B_37', 'B_9', 'D_74', 'B_3', 'D_55', 'B_22', 'B_33', 'R_1', 'B_38', 'D_75', 'D_44', 'D_48', 'B_2', 'B_7', 'B_18', 'B_30', 'B_1', 'D_61', 'B_4', 'P_2', 'R_10', 'B_17', 'B_23']]
+  data = data_frame[cov_list]
   data = data.fillna(0)
   data_avg = data.groupby(['customer_ID']).mean()
 
   return data_avg
 
 X = data_preprocessing()
-
 Y = df.get_train_label()['target'].to_numpy()
-# Using the training model to fit:
-model = LogisticRegression(solver='liblinear', random_state=1).fit(X, Y)
+# # Using the training model to fit:
+# model = LogisticRegression(solver='liblinear', random_state=1).fit(X, Y)
 
-print(model.predict_proba(test_data_process()))
+# print(model.predict_proba(test_data_process()))
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, random_state=1)
+
+
+# XGBoost MODEL:
+dtrain = xgb.DMatrix(x_train, label=y_train)
+dtest = xgb.DMatrix(x_test, label=y_test)
+
+# Paramter spec
+param = {'max_depth': 2, 'eta': 1, 'objective': 'binary:logistic'}
+param['nthread'] = 4
+param['eval_metric'] = 'auc'
+
+evallist = [(dtest, 'eval'), (dtrain, 'train')]
+num_round = 10
+bst = xgb.train(param, dtrain, num_round, evallist)
+
+ypred = bst.predict(xgb.DMatrix(test_data_process()), iteration_range=(0, bst.best_iteration + 1))
+
+
+
+# LOGISTIC REGRESSION MODEL
+# lr_model = LogisticRegression(solver='liblinear', max_iter=200).fit(x_train, y_train)
+# y_pred = lr_model.predict(x_test)
+
+# print(confusion_matrix(y_test, y_pred))
 
 
 # pd.DataFrame(model.predict_proba(test_data_process())).to_csv(r'solution.csv', index = False)
+pd.DataFrame(pd.DataFrame(ypred).to_csv(r'solution.csv', index = False))
 
 
