aef5d2db775ddde0550704f73d6d88abfed6d196
samyu1204 <87253028+samyu1204@users.noreply.github.com>
2022-07-21 21:30:35 +1000

    Merged data set

--- data_cv/df.py
+++ data_cv/df.py
@@ -23,7 +23,7 @@ col_avg = [0.64833, 0.1517, 0.12663, 0.62104, 0.08019, 0.23053, 0.0586, 0.1279, 
 
 # Data getter functions:
 def get_sample_sub():
-  return dd.read_csv("../data/sample_submission.csv")
+  return pd.read_csv("../data/sample_submission.csv")
 
 def get_test_data():
   return pd.read_parquet("../data/test.parquet")
@@ -48,7 +48,7 @@ def get_merge_train_data():
     data[col].fillna(col_avg[i])
     i += 1
 
-  return data
+  return data.groupby(['customer_ID']).mean()
 
 def get_column_avg():
   avg = ['hello']
@@ -69,8 +69,6 @@ def get_column_avg():
   
   print(avg)
   return
-
-
 # * ========================================================================================
 
 # * ========================================================================================
